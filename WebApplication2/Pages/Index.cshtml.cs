d25721de243d367a058fcaddd161188816c0f117
Anna Wyrwal <202340@edu.p.lodz.pl>
2018-05-27 14:12:00 +0200

    Refactor

--- WebApplication2/Pages/Index.cshtml.cs
+++ WebApplication2/Pages/Index.cshtml.cs
@@ -1,27 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using MigsModernization.Context;
-using MigsModernization.Models;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MigsModernization.Pages
 {
     public class IndexModel : PageModel
     {
-        private Sin79_MigsModernizationContext migsModernizationContext;
-        private readonly DbSet<Mig> migs;
-
-        public IndexModel()
-        {
-            migsModernizationContext = new Sin79_MigsModernizationContext();
-            migsModernizationContext.Database.EnsureCreated();
-            migs = migsModernizationContext.Migs;
-        }
-
         public void OnGet()
         {
 
